ff0e905b8fab676b5ed2828591299500648880ec
莫钡先 <beixian.mo@qlchat.com>
2025-08-05 10:40:20 +0800

    直接默认首页

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,21 +1,39 @@
-import { Inter } from "next/font/google";
-import Footer from "@/containers/landing/Footer";
-import Header from "@/containers/landing/Header";
+// import { Inter } from "next/font/google";
+// import Footer from "@/containers/landing/Footer";
+// import Header from "@/containers/landing/Header";
+//
+// const inter = Inter({ subsets: ["latin"] });
+//
+// export default function HomeLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <main className={`flex flex-col items-center min-h-screen ${inter.className}`}>
+//       <Header />
+//       <div className="flex flex-col flex-1 justify-center items-center w-full">
+//         <div className="relative flex flex-1 w-full max-w-[1280px] items-center">{children}</div>
+//       </div>
+//       <Footer />
+//     </main>
+//   );
+// }
 
-const inter = Inter({ subsets: ["latin"] });
+import { CookiesProvider } from "next-client-cookies/server";
+import { getLocale, getTranslations } from "next-intl/server";
 
-export default function HomeLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export async function generateMetadata() {
+  const locale = await getLocale();
+  const t = await getTranslations({ locale, namespace: "Metadata" });
+
+  return { title: t("Editor") };
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className={`flex flex-col items-center min-h-screen ${inter.className}`}>
-      <Header />
-      <div className="flex flex-col flex-1 justify-center items-center w-full">
-        <div className="relative flex flex-1 w-full max-w-[1280px] items-center">{children}</div>
-      </div>
-      <Footer />
+    <main className="w-screen h-screen">
+      <CookiesProvider>{children}</CookiesProvider>
     </main>
   );
 }
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,34 @@
-import FAQ from "@/containers/landing/FAQ";
-import Features from "@/containers/landing/Features";
-import HeroTitle from "@/containers/landing/HeroTitle";
-import { Pricing } from "@/containers/pricing";
+// import FAQ from "@/containers/landing/FAQ";
+// import Features from "@/containers/landing/Features";
+// import HeroTitle from "@/containers/landing/HeroTitle";
+// import { Pricing } from "@/containers/pricing";
+//
+// export default function Index() {
+//   return (
+//     <div className="relative flex flex-col items-center w-full h-full md:mx-32 mx-4 mb-48 space-y-24 text-center">
+//       <HeroTitle />
+//       <Features />
+//       <Pricing />
+//       <FAQ />
+//     </div>
+//   );
+// }
 
-export default function Index() {
+import { Separator } from "@/components/ui/separator";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import MainPanel from "@/containers/editor/MainPanel";
+import SideNav from "@/containers/editor/sidenav";
+import { PricingOverlay } from "@/containers/pricing";
+
+export default async function Page() {
   return (
-    <div className="relative flex flex-col items-center w-full h-full md:mx-32 mx-4 mb-48 space-y-24 text-center">
-      <HeroTitle />
-      <Features />
-      <Pricing />
-      <FAQ />
-    </div>
+    <TooltipProvider delayDuration={0}>
+      <div className="flex h-full w-full">
+        <SideNav />
+        <Separator orientation="vertical" />
+        <MainPanel />
+        <PricingOverlay />
+      </div>
+    </TooltipProvider>
   );
 }
